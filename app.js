3348e3eb69af9dfabaaac60cc6348360dfacff1f
nvaroqui-glitch <n.varoqui@gmail.com>
2026-01-25 17:55:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 console.log("app.js chargé");
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 /** ================================
  *  CONFIG (à remplacer)
  *  ================================ */
