7de902eae35aab64e04b835aa6ef47182a7aca1f
nvaroqui-glitch <n.varoqui@gmail.com>
2026-01-25 14:25:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 console.log("app.js chargé");
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/esm/supabase.js";
 /** ================================
  *  CONFIG (à remplacer)
  *  ================================ */
